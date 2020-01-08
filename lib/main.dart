775863c0ad301cf50761978f249faf5931745d48
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-01-08 15:02:46 +0100

    Questions change on button clicked

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,30 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+ @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>{
+  var questionIndex = 0;
+
+  void answerQuestion(){
+    setState(() {
+      questionIndex = questionIndex + 1 ;
+    });
+    print(questionIndex);
+}
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favourite color?',
-      'What\'s your favourite animal',
+      'What\'s your favourite animal?',
     ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -18,22 +35,26 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(
+              questions[questionIndex],
+            ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
           ],
         ),
       ),
     );
+
   }
 }
+
